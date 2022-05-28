40a671fcdf9dafd559c87b231f21482903bc835b
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-05-28 22:46:41 +0600

    Complete Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Pages/Authentication/Login/Login';
+import Home from './Pages/Home/Home/Home';
+import Navbar from './Pages/Shared/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
     </div>
   );
 }
