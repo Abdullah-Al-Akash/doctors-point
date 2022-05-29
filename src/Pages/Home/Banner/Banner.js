e6c47a762dee72ddcb6ac81babdf290caccc5f1d
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-05-30 00:25:23 +0600

    Banner Complete

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import chair from '../../../assets/images/chair.png';
+
+const Banner = () => {
+        return (
+                <div className="bg-green-100">
+                        <div class="hero min-h-screen container mx-auto">
+                                <div class="hero-content flex-col lg:flex-row-reverse">
+                                        <div className="w--lg-50">
+                                                <img src={chair} class="max-w-full rounded-lg shadow-2xl" />
+                                        </div>
+                                        <div>
+                                                <h1 class="text-5xl font-bold">Your New Smile from Here</h1>
+                                                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                                <button class="btn btn-primary">Get Started</button>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+        );
+};
+
+export default Banner;
